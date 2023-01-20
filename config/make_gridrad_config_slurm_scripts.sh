db0e0493b5b534405f435495ff17a36b4d5267d7
Zhe Feng <zhe.feng@pnnl.gov>
2023-01-20 10:48:50 -0800

    Added scripts to make quicklook plots for Tb+Radar MCS tracking.
    These scripts are made to work with GridRad MCS tracking database over the U.S. produced using the previous IDL version of FLEXTRKR.  Some of the tracking statistics variable names are slightly different from PyFLEXTRKR.  They will need to be updated after Tb+Radar MCS tracking has been implemented in PyFLEXTRKR.
    
    1. Added plot_subset_tbze_mcs_tracks_demo.py to make quicklook plots of Tb + Radar Reflectivity with MCS track paths.
    2. Added make_gridrad_config_slurm_scripts.sh and config_gridrad_mcs_template.yml to make config files for GridRad MCS tracking for each year.
    3. Added make_mcs_quicklook_joblib.py to make joblib slurm scripts for each year
    4. Added make_gridrad_mcstracks_animation.py to make MP4 animations from the quicklook plots

---
+++ config/make_gridrad_config_slurm_scripts.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Create GridRad MCS tracking config and slurm scripts
+
+START_YEAR=2004
+END_YEAR=2017
+
+submit_job="no"
+
+config_dir="/global/homes/f/feng045/program/PyFLEXTRKR/config/"
+slurm_dir="/global/homes/f/feng045/program/PyFLEXTRKR/slurm/"
+
+config_template=${config_dir}"config_gridrad_mcs_template.yml"
+
+slurm_template=${slurm_dir}"slurm_lasso_wrf100m_template.sh"
+
+config_basename="config_gridrad_mcs_"
+slurm_basename="slurm_gridrad_mcs_"
+
+# Loop over year
+for year in $(seq $START_YEAR $END_YEAR); do
+    # Start/end date for tracking period
+    # sdate=${year}0401.0000
+    # edate=${year}0831.0000
+    sdate=${year}0101.0000
+    edate=${year}1231.0000
+
+    config_file=${config_dir}${config_basename}${year}.yml
+    # slurm_file=${slurm_dir}${slurm_basename}${sdate}_${edate}.sh
+
+    sed "s/STARTDATE/"${sdate}"/g;s/ENDDATE/"${edate}"/g" $config_template > ${config_file}
+    # sed "s/STARTDATE/"${sdate}"/g;s/ENDDATE/"${edate}"/g" $slurm_template > ${slurm_file}
+    echo ${config_file}
+    # echo ${slurm_file}
+
+    # # Submit job
+    # if [[ "${submit_job}" == "yes" ]]; then
+    #     sbatch ${slurm_file}
+done
