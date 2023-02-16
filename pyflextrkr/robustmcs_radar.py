f387b92e90fb18e65270ccf58864920b39bc3114
jli628 <jianfeng.li@gatech.edu>
2023-02-15 17:05:33 -0800

    MCS lifecycle identification added in pyflextrkr/robustmcs_radar.py

--- pyflextrkr/robustmcs_radar.py
+++ pyflextrkr/robustmcs_radar.py
@@ -5,6 +5,7 @@ import sys
 import time
 import warnings
 import logging
+import pandas as pd
 
 def define_robust_mcs_radar(config):
     """
@@ -71,7 +72,7 @@ def define_robust_mcs_radar(config):
 
     # pf_mcstype = np.full(ntracks, fillval, dtype=int)
     pf_mcsstatus = np.full((ntracks, ntimes), fillval, dtype=int)
-    pf_cctype = np.full((ntracks, ntimes), fillval, dtype=int)
+    #pf_cctype = np.full((ntracks, ntimes), fillval, dtype=int)
 
     ###################################################
     # Loop through each track
@@ -208,7 +209,169 @@ def define_robust_mcs_radar(config):
         # import pdb;
         # pdb.set_trace()
 
+        #mcs_basetime = np.empty((nmcs, ntimes), dtype="datetime64[s]")
 
+        # Loop through each mcs
+        for ilm in range(0, nlongmcs):
+            # Initialize arrays
+            ilm_index = np.ones(5, dtype=int) * fillval
+            ilm_cycle = np.ones(ntimes, dtype=int) * fillval
+
+            # Isolate data from this track
+            ilm_irtracklength = np.copy(ir_trackduration[ilongmcs[ilm]]).astype(int)
+            ilm_pfccmajoraxis = np.copy(pf_coremajoraxis[ilongmcs[ilm], 0:ilm_irtracklength, :])
+            ilm_pfccarea = np.copy(pf_corearea[ilongmcs[ilm], 0:ilm_irtracklength, :])
+            ilm_meansfarea = np.sum(np.copy(pf_sfarea[ilongmcs[ilm], 0:ilm_irtracklength, :]), axis=1)
+            ilm_pfarea = np.copy(pf_area[ilongmcs[ilm], 0:ilm_irtracklength, 0])
+
+            ilm_maxpfccmajoraxis = np.nanmax(ilm_pfccmajoraxis, axis=1)
+            ilm_maxpfccarea = np.nanmax(ilm_pfccarea, axis=1)
+
+            #TEMP_basetime = np.array(
+            #        [
+            #            pd.to_datetime(
+            #                ds_pf["basetime"][
+            #                    trackid_mcs[ilongmcs[ilm]], 0:ilm_irtracklength
+            #                ].data,
+            #                unit="s",
+            #            )
+            #        ]
+            #)
+            #mcs_basetime[ilm, 0:ilm_irtracklength] = TEMP_basetime
+
+            ##################################################################
+            # Find indices of when convective line present and absent and when stratiform present
+
+            # Find times with convective core area > 0
+            iccarea = np.array(np.where(ilm_maxpfccarea > 0))[0, :]
+            iccarea_groups = np.split(iccarea, np.where(np.diff(iccarea) > 2)[0] + 1)
+            if len(iccarea) > 0 and len(iccarea_groups) > 1:
+                grouplength = np.empty(len(iccarea_groups))
+                for igroup in range(0, len(iccarea_groups)):
+                    grouplength[igroup] = len(iccarea_groups[igroup][:])
+                maxgroup = np.nanargmax(grouplength)
+                iccarea = iccarea_groups[maxgroup][:]
+            elif len(iccarea) > 0:
+                iccarea = np.arange(iccarea[0], iccarea[-1] + 1)
+            nccarea = len(iccarea)
+
+            # Find times with convective major axis length greater than 100 km
+            iccline = np.array(np.where(ilm_maxpfccmajoraxis > 100))[0, :]
+            iccline_groups = np.split(iccline, np.where(np.diff(iccline) > 2)[0] + 1)
+            if len(iccline) > 0 and len(iccline_groups) > 1:
+                grouplength = np.empty(len(iccline_groups))
+                for igroup in range(0, len(iccline_groups)):
+                    grouplength[igroup] = len(iccline_groups[igroup][:])
+                maxgroup = np.nanargmax(grouplength)
+                iccline = iccline_groups[maxgroup][:]
+            elif len(iccline) > 0:
+                iccline = np.arange(iccline[0], iccline[-1] + 1)
+            nccline = len(iccline)
+
+            # Find times with convective major axis length greater than 100 km and stratiform area greater than the median amount of stratiform
+            # ilm_meansfarea[ilm_meansfarea == fillvalue] = np.nan
+            print(ilm_maxpfccmajoraxis.shape)
+            print(ilm_meansfarea.shape)
+            isfarea = np.array(np.where((ilm_maxpfccmajoraxis > 100) & (ilm_meansfarea > np.nanmean(ilm_meansfarea))))[0, :]
+            isfarea_groups = np.split(isfarea, np.where(np.diff(isfarea) > 2)[0] + 1)
+            if len(isfarea) > 0 and len(isfarea_groups) > 1:
+                grouplength = np.empty(len(isfarea_groups))
+                for igroup in range(0, len(isfarea_groups)):
+                    grouplength[igroup] = len(isfarea_groups[igroup][:])
+                maxgroup = np.nanargmax(grouplength)
+                isfarea = isfarea_groups[maxgroup][:]
+            elif len(isfarea) > 0:
+                isfarea = np.arange(isfarea[0], isfarea[-1] + 1)
+            nsfarea = len(isfarea)
+
+            # Find times with convective major axis length less than 100 km
+            #if nsfarea > 0:
+            #    inoccline = np.array(np.where(ilm_maxpfccmajoraxis < 100))[0, :]
+            #    inoccline = inoccline[
+            #            np.where((inoccline > isfarea[-1]) & (inoccline > iccline[-1]))
+            #    ]
+            #    inoccline_groups = np.split(
+            #            inoccline, np.where(np.diff(inoccline) > 2)[0] + 1
+            #    )
+            #    if len(inoccline) > 0 and len(inoccline_groups) > 1:
+            #        grouplength = np.empty(len(inoccline_groups))
+            #        for igroup in range(0, len(inoccline_groups)):
+            #            grouplength[igroup] = len(inoccline_groups[igroup][:])
+            #        maxgroup = np.nanargmax(grouplength)
+            #        inoccline = inoccline_groups[maxgroup][:]
+            #    elif len(inoccline) > 0:
+            #        inoccline = np.arange(inoccline[0], inoccline[-1] + 1)
+            #    nnoccline = len(inoccline)
+
+            ###############################################################################
+            # Classify cloud only stage
+
+            # Cloud only stage
+            if nccarea > 0:
+                # If first convective time is after the first cloud time, label all hours before the convective core appearance time as preconvective
+                if iccarea[0] > 0 and iccarea[0] < ilm_irtracklength - 1:
+                    ilm_index[0] = 0  # Start of cloud only
+                    ilm_cycle[0 : iccarea[0]] = 1  # Time period of cloud only
+
+                ilm_index[1] = iccarea[0]  # Start of unorganized convective cells
+
+            # If convective line exists
+            if nccline > 1:
+                # If the convective line occurs after the first storm time (use second index since convective line must be around for one hour prior to classifying as genesis)
+                # Label when convective cores first appear, but are not organized into a line
+                if iccline[1] > iccarea[0]:
+                    ilm_index[2] = iccline[1]  # Start of organized convection
+                    ilm_cycle[iccarea[0] : iccline[1]] = 2  # Time period of unorganzied convective cells
+                else:
+                    sys.exit(
+                            "Check convective line in track " + str(int(ilongmcs[ilm]))
+                    )
+
+                if nsfarea > 0:
+                    # Label MCS genesis. Test if stratiform area time is two timesteps after the convective line and two time steps before the last time of the cloud track
+                    if isfarea[0] > iccline[1] + 2:
+                        ilm_index[3] = isfarea[0]  # Start of mature MCS
+                        ilm_cycle[iccline[1] : isfarea[0]] = 3  # Time period of organized cells before maturation
+
+                        ilm_cycle[isfarea[0] : isfarea[-1] + 1] = 4  # Time period of mature mcs
+                    else:
+                        matureindex = isfarea[np.array(np.where(isfarea == iccline[1] + 2))[0, :]]
+                        if len(matureindex) > 0:
+                            ilm_index[3] = np.copy(matureindex[0])
+                            ilm_cycle[iccline[1] : matureindex[0]] = 3  # Time period of organized cells before maturation
+
+                            ilm_cycle[matureindex[0] : isfarea[-1] + 1] = 4  # Time period of mature mcs
+
+                            # if isfarea[0] > iccline[1] + 2
+                            #    ilm_index[3] = isfarea[0] # Start of mature MCS
+                            #    ilm_cycle[iccline[1]:isfarea[0]] = 3 # Time period of organized cells before maturation
+                            #    ilm_cycle[isfarea[0]:isfarea[-1]+1] = 4 # Time period of mature mcs
+                            # else:
+                            #    if nsfarea > 3:
+                            #        ilm_index[3] = isfarea[3] # Start of mature MCS
+                            #        ilm_cycle[iccline[1]:isfarea[3]] = 3 # Time period of organized cells before maturation
+                            #        ilm_cycle[isfarea[3]:isfarea[-1]+1] = 4 # Time period of mature MCS
+                            # Label dissipating times. Buy default this is all times after the mature stage
+                    if isfarea[-1] < ilm_irtracklength - 1:
+                        ilm_index[4] = isfarea[-1] + 1    #as long as nsfarea > 0, the dissipation stage can be defined
+                        ilm_cycle[isfarea[-1] + 1 : ilm_irtracklength] = 5  # Time period of dissipation
+
+            ############################################################
+            # Final life cycle processing
+            istage = np.array(np.where(ilm_cycle >= 0))[0, :]
+            nstage = len(istage)
+
+            if nstage > 0:
+                cyclepresent = np.copy(ilm_cycle[istage])
+                uniquecycle = np.unique(cyclepresent)
+
+                # Label as complete cycle if 1-4 present
+                if len(uniquecycle) >= 4:
+                    cycle_complete[ilongmcs[ilm]] = 1
+
+                # Save data
+                cycle_stage[ilongmcs[ilm], :] = np.copy(ilm_cycle)
+                cycle_index[ilongmcs[ilm], :] = np.copy(ilm_index)
 
     # Subset robust MCS tracks from PF dataset
     # Note: the tracks_dimname cannot be used here as Xarray does not seem to have
@@ -218,6 +381,8 @@ def define_robust_mcs_radar(config):
     tracks_coord = np.arange(0, nmcs)
     times_coord = ds_pf[times_dimname]
     dsout[tracks_dimname] = tracks_coord
+    lifestage_coord = np.arange(0, 5)
+    dsout["lifestages"] = lifestage_coord
 
     # Convert new variables to DataArrays
     pf_lifetime = xr.DataArray(
@@ -229,6 +394,18 @@ def define_robust_mcs_radar(config):
             "units": "hour",
        }
     )
+
+    cycle_complete = xr.DataArray(
+            cycle_complete,
+            coords={tracks_dimname:tracks_coord},
+            dims=(tracks_dimname),
+            attrs={
+                "long_name": "Flag indicating if this MCS has each element in the MCS life cycle",
+                "units": "unitless",
+                "_FillValue": fillval,
+            }
+    )
+
     pf_mcsstatus = xr.DataArray(
         pf_mcsstatus,
         coords={tracks_dimname:tracks_coord, times_dimname:times_coord},
@@ -240,9 +417,36 @@ def define_robust_mcs_radar(config):
         }
     )
 
+    cycle_stage = xr.DataArray(
+            cycle_stage,
+            coords={tracks_dimname:tracks_coord, times_dimname:times_coord},
+            dims=(tracks_dimname, times_dimname),
+            attrs={
+                "long_name": "Each time in the MCS is labeled with a flag indicating its phase in the MCS lifecycle. 1 = Cloud only, 2 = Isolated convective cores, 3 = MCS genesis, 4 = MCS maturation, 5 = MCS decay",
+                "units": "unitless",
+                "_FillValue": fillval,
+            }
+    )
+
+    cycle_index = xr.DataArray(
+            cycle_index,
+            coords={tracks_dimname:tracks_coord, "lifestages":lifestage_coord},
+            dims=(tracks_dimname, "lifestages"),
+            attrs={
+                "long_name": "Time index when each phase of the MCS life cycle starts",
+                "units": "unitless",
+                "_FillValue": fillval,
+            }
+    )
+
+    print("OK ljf")
+
     # Add new variables to dataset
     dsout["pf_lifetime"] = pf_lifetime
     dsout["pf_mcsstatus"] = pf_mcsstatus
+    dsout["lifecycle_stage"] = cycle_stage
+    dsout["lifecycle_complete_flag"] = cycle_complete
+    dsout["lifecycle_index"] = cycle_index
 
     # Update global attributes
     dsout.attrs["MCS_PF_majoraxis_thresh"] = mcs_pf_majoraxis_thresh
