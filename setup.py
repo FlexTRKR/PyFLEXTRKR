e53ebfc84fdef85d472c25bb89e291d2bedcbceb
Zhe Feng <zhe.feng@pnnl.gov>
2023-02-18 17:19:14 -0800

    Updated URL in setup.py.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     description="A Python package for atmospheric feature tracking.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/FlexTRKR/PyFLEXTRKR",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
