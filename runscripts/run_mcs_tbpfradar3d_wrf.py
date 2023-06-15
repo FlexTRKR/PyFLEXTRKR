d7d14e25c1d9386eec5fa6413f90f67ec3dc9df8
candiceT233 <candicet233@gmail.com>
2023-06-14 19:38:30 -0500

    flush clients memory each stage

--- runscripts/run_mcs_tbpfradar3d_wrf.py
+++ runscripts/run_mcs_tbpfradar3d_wrf.py
@@ -15,6 +15,16 @@ from pyflextrkr.robustmcs_radar import define_robust_mcs_radar
 from pyflextrkr.mapfeature_driver import mapfeature_driver
 from pyflextrkr.movement_speed import movement_speed
 
+def restart_dask_cluster(client,cluster):
+    client.shutdown()
+    ################################################################################################
+    # Parallel processing options
+    if config['run_parallel'] == 1:
+        client = Client(cluster)
+        client.run(setup_logging)
+    
+
+
 if __name__ == '__main__':
 
     # Set the logging message level
@@ -61,30 +71,61 @@ if __name__ == '__main__':
 
     # Step 2 - Link features in time adjacent files
     if config['run_tracksingle']:
+        if config['run_parallel'] == 1:
+            client.shutdown()
+            dask_tmp_dir = config.get("dask_tmp_dir", "./")
+            dask.config.set({'temporary-directory': dask_tmp_dir})
+            # Local cluster
+            cluster = LocalCluster(n_workers=config['nprocesses'], threads_per_worker=1)
+            client = Client(cluster)
         tracksingle_driver(config)
 
     # Step 3 - Track features through the entire dataset
     if config['run_gettracks']:
+        if config['run_parallel'] == 1:
+            client.shutdown()
+            cluster = LocalCluster(n_workers=config['nprocesses'], threads_per_worker=1)
+            client = Client(cluster)
         tracknumbers_filename = gettracknumbers(config)
 
     # Step 4 - Calculate track statistics
     if config['run_trackstats']:
+        if config['run_parallel'] == 1:
+            client.shutdown()
+            cluster = LocalCluster(n_workers=config['nprocesses'], threads_per_worker=1)
+            client = Client(cluster)
         trackstats_filename = trackstats_driver(config)
 
     # Step 5 - Identify MCS using Tb
     if config['run_identifymcs']:
+        if config['run_parallel'] == 1:
+            client.shutdown()
+            cluster = LocalCluster(n_workers=config['nprocesses'], threads_per_worker=1)
+            client = Client(cluster)
         mcsstats_filename = identifymcs_tb(config)
 
     # Step 6 - Match PF to MCS
     if config['run_matchpf']:
+        if config['run_parallel'] == 1:
+            client.shutdown()
+            cluster = LocalCluster(n_workers=config['nprocesses'], threads_per_worker=1)
+            client = Client(cluster)
         pfstats_filename = match_tbpf_tracks(config)
 
     # Step 7 - Identify robust MCS
     if config['run_robustmcs']:
+        if config['run_parallel'] == 1:
+            client.shutdown()
+            cluster = LocalCluster(n_workers=config['nprocesses'], threads_per_worker=1)
+            client = Client(cluster)
         robustmcsstats_filename = define_robust_mcs_radar(config)
 
     # Step 8 - Map tracking to pixel files
     if config['run_mapfeature']:
+        if config['run_parallel'] == 1:
+            client.shutdown()
+            cluster = LocalCluster(n_workers=config['nprocesses'], threads_per_worker=1)
+            client = Client(cluster)
         # Map robust MCS track numbers to pixel files (default)
         mapfeature_driver(config, trackstats_filebase=mcsrobust_filebase)
         # # Map Tb-only MCS track numbers to pixel files (provide outpath_basename keyword)
@@ -94,4 +135,7 @@ if __name__ == '__main__':
 
     # Step 9 - Movement speed calculation
     if config['run_speed']:
+        if config['run_parallel'] == 1:
+            cluster = LocalCluster(n_workers=config['nprocesses'], threads_per_worker=1)
+            client = Client(cluster)
         movement_speed(config)
